20d7af83835116831433a87ea6465a92a25e4230
Michael Chirico <MichaelChirico4@gmail.com>
2016-12-05 19:30:29 -0500

    updating bash per Pau's instructions

--- pei_pai_scripts/rotation.bash
+++ pei_pai_scripts/rotation.bash
@@ -1,4 +1,10 @@
-#! /bin/bash
+#!/bin/bash
+#$ -cwd
+#$ -q all.q
+#$ -pe openmp 1
+#$ -S /bin/bash
+#$ -o /home/chiricom/portland/pei_pai_scripts/output_rotate.log
+#$ -e /home/chiricom/portland/pei_pai_scripts/error_rotate.log
 ./grid_rotation.R 250 250 1w
 ./grid_rotation.R 250 250 2w
 ./grid_rotation.R 250 250 1m
