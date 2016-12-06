3cc83a5b2795de70d07bd8778a2a46e468435532
Michael Chirico <MichaelChirico4@gmail.com>
2016-12-05 19:04:26 -0500

    Scripts for grid rotation

---
+++ pei_pai_scripts/rotation.bash
@@ -0,0 +1,15 @@
+#! /bin/bash
+./grid_rotation.R 250 250 1w
+./grid_rotation.R 250 250 2w
+./grid_rotation.R 250 250 1m
+./grid_rotation.R 250 250 2m
+
+./grid_rotation.R 500 500 1w
+./grid_rotation.R 500 500 2w
+./grid_rotation.R 500 500 1m
+./grid_rotation.R 500 500 2m
+
+./grid_rotation.R 600 600 1w
+./grid_rotation.R 600 600 2w
+./grid_rotation.R 600 600 1m
+./grid_rotation.R 600 600 2m
