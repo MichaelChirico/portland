82fd80effd4547d421185926cd9158684c694999
Michael Chirico <MichaelChirico4@gmail.com>
2016-12-29 16:13:59 -0500

    en-scripting, part 4 -- nearing bare-minimum completion

--- create_vw.R
+++ create_vw.R
@@ -16,8 +16,17 @@ features = as.integer(args[4L])
 l2 = as.numeric(args[5L])
 l1 = as.numeric(args[6L])
 alpha = as.numeric(args[7L])
+metric = args[8L]
+horizon = list('1w' = as.Date(c('2016-03-01', '2016-03-06')),
+               '2w' = as.Date(c('2016-03-01', '2016-03-13')),
+               '1m' = as.Date(c('2016-03-01', '2016-03-31')),
+               '2m' = as.Date(c('2016-03-01', '2016-04-30')),
+               '3m' = as.Date(c('2016-03-01', '2016-05-21')))[[args[9L]]]
+
+aa = delx*dely #forecasted area
 
 crimes = fread("crimes.csv")
+crimes[ , occ_date := as.IDate(occ_date)]
 
 #record range here, so that
 #  we have the same range 
@@ -39,16 +48,14 @@ crimes_ever[order(x, y), I := .I]
 #eliminate no-crime cells
 crimes_ever = crimes_ever[value > 0]
 
-#now subset to desired date range
-crimes = crimes[as.IDate(occ_date) %between%
-                  c("2016-02-22", "2016-02-28")]
-
 #repeat over-cell counting
 crimes.grid.dt = 
-  with(crimes, setDT(as.data.frame(pixellate(ppp(
-    x = x_coordina, y = y_coordina,
-    xrange = xrng, yrange = yrng),
-    dimyx = c(y = dely, x = delx)))))
+  with(crimes[occ_date %between%
+                c("2016-02-22", "2016-02-28")],
+       setDT(as.data.frame(pixellate(ppp(
+         x = x_coordina, y = y_coordina,
+         xrange = xrng, yrange = yrng),
+         dimyx = c(y = dely, x = delx)))))
 crimes.grid.dt[order(x, y), I := .I]
 
 #subset to eliminate never-crime cells
@@ -96,8 +103,28 @@ crimes.grid.dt[preds, pred := i.pred, on = "I"]
 #when we're at the minimum forecast area, we must round up
 #  to be sure we don't undershoot; when at the max,
 #  we must round down; otherwise, just round
+# **TO DO: if we predict any boundary cells and are using the minimum
+#          forecast area, WE'LL FALL BELOW IT WHEN WE CLIP TO PORTLAND **
 which.round = function(x) if (x > 0) {if (x < 1) round else floor} else ceiling
 
-n.cells = as.integer(which.round(alpha)(6969600*(1+2*alpha)/delx/dely))
+n.cells = as.integer(which.round(alpha)(6969600*(1+2*alpha)/aa))
 
 crimes.grid.dt[order(-pred), hotspot := .I <= n.cells]
+
+#now, pixellate the future crimes
+crimes.future = 
+  with(crimes[occ_date %between% horizon],
+       setDT(as.data.frame(pixellate(ppp(
+         x = x_coordina, y = y_coordina,
+         xrange = xrng, yrange = yrng),
+         dimyx = c(y = dely, x = delx)))))
+crimes.future[order(x, y), I := .I]
+
+#how well did we do? lower-case n in the PEI/PAI calculation
+nn = crimes.future[crimes.grid.dt[(hotspot)], sum(x.value), on = "I"]
+
+switch(metric,
+       pei = nn/crimes.future[order(-value)[1L:n.cells], sum(value)],
+       #rather than load the portland shapefile just to calculate
+       #  the total area, pre-do that here
+       pai = (nn/crimes.future[ , sum(value)])/(aa*n.cells/4117777129))
--- fit_features.sh
+++ fit_features.sh
@@ -9,6 +9,13 @@
 #    5) L2 loss parameter for VW
 #    6) L1 loss parameter for VW
 #    7) forecast area target weight alpha:
-#         alpha = 0: minimum forecast area
-#         alpha = 1: maximum forecast area
-./create_vw.R 600 600 1800 20 1e-4 1e-5 0
+#         alpha = 0 : minimum forecast area
+#         alpha = 1 : maximum forecast area
+#    8) competition metric (pei or pai)
+#    9) forecasting horizon h:
+#         h = 1w : one week
+#         h = 2w : two weeks
+#         h = 1m : one month
+#         h = 2m : two months
+#         h = 3m : three months
+./create_vw.R 600 600 1800 20 1e-4 1e-5 0 pei 1w
