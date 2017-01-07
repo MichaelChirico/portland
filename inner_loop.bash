01c84b21998ada9a264a0e28c312796fd9797d37
Michael Chirico <MichaelChirico4@gmail.com>
2017-01-07 18:57:45 -0500

    so much for that

--- inner_loop.bash
+++ inner_loop.bash
@@ -10,16 +10,4 @@ source /etc/profile.d/modules.sh
 module unload R/3.2.3
 module load shared R/3.3.2 vowpal_wabbit gcc/5.2.0
 
-for dx in 250 425 600
-do
-	for dy in 250 425 600
-	do
-		for eta in 0 3 6
-		do
-			for lt in 0 3 6
-			do
-				sh fit_features.bash $dx $dy 0 $eta $lt 200 1e-5 1e-4 .5 1 0 .5 "$@"
-			done
-		done
-	done
-done
+sh fit_features.bash 600 600 0 1 2 200 1e-5 1e-4 .5 1 0 .5 "$@"
