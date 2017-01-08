d729022a03788118c12e36bfdb1841d30daa1f3d
Michael Chirico <MichaelChirico4@gmail.com>
2017-01-08 14:37:54 -0500

    revert to full loop and cross fingers

--- inner_loop.bash
+++ inner_loop.bash
@@ -3,12 +3,24 @@
 #$ -q all.q
 #$ -pe openmp 1
 #$ -S /bin/bash
-#$ -o /home/chiricom/portland/output_vw_fit3.log
-#$ -e /home/chiricom/portland/error_vw_fit3.log
+#$ -o /home/chiricom/portland/output_vw_fit.log
+#$ -e /home/chiricom/portland/error_vw_fit.log
 
 source /etc/profile.d/modules.sh
 module unload R/3.2.3
 module load shared R/3.3.2 vowpal_wabbit gcc/5.2.0
 unset R_HOME
 
-sh fit_features.bash 600 600 0 1 2 200 1e-5 1e-4 .5 1 0 .5 "$@"
+for dx in 250 425 600
+do
+	for dy in 250 425 600
+	do
+		for eta in 0 3 6
+		do
+			for lt in 0 3 6
+			do
+				sh fit_features.bash $dx $dy 0 $eta $lt 200 1e-5 1e-4 .5 1 0 .5 "$@"
+			done
+		done
+	done
+done
