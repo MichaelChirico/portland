f27aec167f9bd688ce651f8c664e9ca5ad1e2631
Michael Chirico <MichaelChirico4@gmail.com>
2017-01-08 15:45:32 -0500

    whoops division by 0

--- inner_loop.bash
+++ inner_loop.bash
@@ -15,9 +15,9 @@ for dx in 250 425 600
 do
 	for dy in 250 425 600
 	do
-		for eta in 0 3 6
+		for eta in 1 3 6
 		do
-			for lt in 0 3 6
+			for lt in 1 3 6
 			do
 				sh fit_features.bash $dx $dy 0 $eta $lt 200 1e-5 1e-4 .5 1 0 .5 "$@"
 			done
