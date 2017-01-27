dd5688efdae8407ca184376d3a85dbf6878d5409
Michael Chirico <MichaelChirico4@gmail.com>
2017-01-26 19:33:44 -0500

    adjusting code to produce new plots from new runs

--- vary_plots.R
+++ vary_plots.R
@@ -3,11 +3,10 @@ library(funchir)
 
 scores = 
   fread("scores/all_1w.csv"
-        )[delx %in% c(250, 425) & dely %in% c(425, 600) & 
-            alpha == 0 & eta %in% 4:6 & lt %in% (1:3 + .5)]
+        )[alpha == 0 & eta %between% c(1.5, 3.5) & 
+            lt %between% c(4, 6)]
 
 timings = fread("timings/all_1w.csv")
-MAXT = timings[p == .5, max(time)]
 
 dims = scores[ , CJ(delx = delx, dely = dely, 
                     unique = TRUE)][order(-dely, delx)]
@@ -68,9 +67,12 @@ timings[delx == 425 & dely == 600 & k == 200 & l1 == 1e-5 &
 
 # FEATURES ####
 scores_k = 
-  scores[delta == 1 & l1 == 1e-5 & l2 == 1e-4 & lambda == .5 & 
+  scores[delx %in% c(250, 425) & dely %in% c(425, 600) & 
+           delta == 1 & l1 == 1e-5 & l2 == 1e-4 & lambda == .5 & 
            t0 == 0 & p == .5][ , .(pei = max(pei), pai = max(pai)),
                                by = .(delx, dely, eta, lt, k)]
+dims_k = scores_k[ , CJ(delx = delx, dely = dely, 
+                        unique = TRUE)][order(-dely, delx)]
 
 scores_k[ , {
   pdf("scores/vary_features.pdf")
@@ -79,16 +81,12 @@ scores_k[ , {
       oma = c(5.1, 4.1, 3.1, 2.1))
   MAX = max(pei)
   for (ii in 1:nrow(dims)) {
-    x = dcast(.SD[dims[ii], on = c("delx", "dely")], 
+    x = dcast(.SD[dims_k[ii], on = c("delx", "dely")], 
               k ~ eta + lt, value.var = "pei")
     y = x[ , !"k"]
-    matplot(x$k, y, type = "l", lty = 1, col = cols[names(y)], lwd = 3,
+    matplot(x$k, y, type = "l", lty = 1, lwd = 3,
             ylab = "", xlab = "", ylim = c(0, MAX), xaxt = "n", yaxt = "n")
     tile.axes(ii, 2L, 2L)
-    if (ii == 3L)
-      legend("bottomright", do.call(paste, c(tstrsplit(names(y), "_"),
-                                            list(sep = "/"))),
-             col = cols[names(y)], lwd = 3, title = "eta/lt", ncol = 2L)
     if (ii < 3L) mtext(paste0("E-W Cell Size: ", dims[ii, delx]), 
                              side = 3L, cex = .5)
           if (ii %% 2L == 0L) mtext(paste0("N-S Cell Size: ", dims[ii, dely]),
@@ -102,22 +100,21 @@ scores_k[ , {
   dev.off()
   }]
 
-timings[delx == 425 & dely == 600 & delta == 1 & l1 == 1e-5 & 
-          l2 == 1e-4 & lambda == .5 & t0 == 0 & p == .5
-        ][ , .(time = mean(time)), keyby = k
-           ][ , {
-             pdf("timings/vary_k.pdf")
-             plot(k, time, type = "l", col = "red", lwd = 3L,
-                  xlab = "# Features", ylab = "Total Time", ylim = c(0, MAXT),
-                  main = "Sensitivity of Run Time to # Features")
-             dev.off()
-           }]
+pdf("timings/vary_k.pdf")
+boxplot(time ~ k, data = timings[delta == 1 & l1 == 1e-5 & l2 == 1e-4 & 
+                                   lambda == .5 & t0 == 0 & p == .5],
+        col = "red", xlab = "# Features", ylab = "Total Time", 
+        main = "Sensitivity of Run Time to # Features")
+dev.off()
 
 # L1 ####
 scores_l1 = 
-  scores[delta == 1 & k == 200 & l2 == 1e-4 & lambda == .5 & 
+  scores[delx %in% c(250, 425) & dely %in% c(425, 600) & 
+           delta == 1 & k == 200 & l2 == 1e-4 & lambda == .5 & 
            t0 == 0 & p == .5][ , .(pei = max(pei), pai = max(pai)),
                                by = .(delx, dely, eta, lt, l1)]
+dims_l1 = scores_l1[ , CJ(delx = delx, dely = dely, 
+                        unique = TRUE)][order(-dely, delx)]
 
 scores_l1[ , {
   pdf("scores/vary_l1.pdf")
@@ -126,16 +123,12 @@ scores_l1[ , {
       oma = c(5.1, 4.1, 3.1, 2.1))
   MAX = max(pei)
   for (ii in 1:nrow(dims)) {
-    x = dcast(.SD[dims[ii], on = c("delx", "dely")], 
+    x = dcast(.SD[dims_l1[ii], on = c("delx", "dely")], 
               l1 ~ eta + lt, value.var = "pei")
     y = x[ , -1L]
-    matplot(log10(x[[1L]]), y, type = "l", lty = 1, col = cols[names(y)], lwd = 3,
+    matplot(log10(x[[1L]]), y, type = "l", lty = 1, lwd = 3,
             ylab = "", xlab = "", ylim = c(0, MAX), xaxt = "n", yaxt = "n")
     tile.axes(ii, 2L, 2L)
-    if (ii == 3L)
-      legend("bottomleft", do.call(paste, c(tstrsplit(names(y), "_"),
-                                            list(sep = "/"))),
-             col = cols[names(y)], lwd = 3, title = "eta/lt", ncol = 2L)
     if (ii < 3L) mtext(paste0("E-W Cell Size: ", dims[ii, delx]), 
                              side = 3L, cex = .5)
           if (ii %% 2L == 0L) mtext(paste0("N-S Cell Size: ", dims[ii, dely]),
@@ -149,22 +142,21 @@ scores_l1[ , {
   dev.off()
   }]
 
-timings[delx == 250 & dely == 600 & k == 200 &  
-          l2 == 1e-4 & lambda == .5 & t0 == 0 & p == .5
-        ][ , .(time = mean(time)), keyby = l1
-           ][ , {
-             pdf("timings/vary_l1.pdf")
-             plot(log10(l1), time, type = "l", col = "red", lwd = 3L,
-                  xlab = "L1", ylab = "Total Time", ylim = c(0, MAXT),
-                  main = "Sensitivity of Run Time to L1")
-             dev.off()
-           }]
+pdf("timings/vary_l1.pdf")
+boxplot(time ~ l1, data = timings[delta == 1 & k == 200 & l2 == 1e-4 & 
+                                   lambda == .5 & t0 == 0 & p == .5],
+        col = "red", xlab = "L1", ylab = "Total Time", 
+        main = "Sensitivity of Run Time to L1")
+dev.off()
 
 # L2 ####
 scores_l2 = 
-  scores[delta == 1 & k == 200 & l1 == 1e-5 & lambda == .5 & 
+  scores[delx %in% c(250, 425) & dely %in% c(250, 425) & 
+           delta == 1 & l1 == 1e-5 & k == 200 & lambda == .5 & 
            t0 == 0 & p == .5][ , .(pei = max(pei), pai = max(pai)),
                                by = .(delx, dely, eta, lt, l2)]
+dims_l2 = scores_l2[ , CJ(delx = delx, dely = dely, 
+                        unique = TRUE)][order(-dely, delx)]
 
 scores_l2[ , {
   pdf("scores/vary_l2.pdf")
@@ -173,16 +165,12 @@ scores_l2[ , {
       oma = c(5.1, 4.1, 3.1, 2.1))
   MAX = max(pei)
   for (ii in 1:nrow(dims)) {
-    x = dcast(.SD[dims[ii], on = c("delx", "dely")], 
+    x = dcast(.SD[dims_l2[ii], on = c("delx", "dely")], 
               l2 ~ eta + lt, value.var = "pei")
     y = x[ , -1L]
-    matplot(log10(x[[1L]]), y, type = "l", lty = 1, col = cols[names(y)], lwd = 3,
+    matplot(log10(x[[1L]]), y, type = "l", lty = 1, lwd = 3,
             ylab = "", xlab = "", ylim = c(0, MAX), xaxt = "n", yaxt = "n")
     tile.axes(ii, 2L, 2L)
-    if (ii == 3L)
-      legend("topright", do.call(paste, c(tstrsplit(names(y), "_"),
-                                            list(sep = "/"))),
-             col = cols[names(y)], lwd = 3, title = "eta/lt", ncol = 2L)
     if (ii < 3L) mtext(paste0("E-W Cell Size: ", dims[ii, delx]), 
                              side = 3L, cex = .5)
           if (ii %% 2L == 0L) mtext(paste0("N-S Cell Size: ", dims[ii, dely]),
@@ -196,22 +184,22 @@ scores_l2[ , {
   dev.off()
   }]
 
-timings[delx == 250 & dely == 600 & k == 200 & l1 == 1e-5 & 
-          lambda == .5 & t0 == 0 & p == .5
-        ][ , .(time = mean(time)), keyby = l2
-           ][ , {
-             pdf("timings/vary_l2.pdf")
-             plot(log10(l2), time, type = "l", col = "red", lwd = 3L,
-                  xlab = "L2", ylab = "Total Time", ylim = c(0, MAXT),
-                  main = "Sensitivity of Run Time to L2")
-             dev.off()
-           }]
+pdf("timings/vary_l2.pdf")
+boxplot(time ~ l2, data = timings[delta == 1 & l1 == 1e-5 & k == 200 & 
+                                   lambda == .5 & t0 == 0 & p == .5],
+        col = "red", xlab = "L2", ylab = "Total Time", 
+        main = "Sensitivity of Run Time to L2")
+dev.off()
+
 
 # LAMBDA ####
 scores_lambda = 
-  scores[delta == 1 & k == 200 & l1 == 1e-5 & l2 == 1e-4 & 
+  scores[delx %in% c(250, 425) & dely %in% c(250, 425) & 
+           delta == 1 & l1 == 1e-5 & l2 == 1e-4 & k == 200 & 
            t0 == 0 & p == .5][ , .(pei = max(pei), pai = max(pai)),
                                by = .(delx, dely, eta, lt, lambda)]
+dims_lambda = scores_lambda[ , CJ(delx = delx, dely = dely, 
+                        unique = TRUE)][order(-dely, delx)]
 
 scores_lambda[ , {
   pdf("scores/vary_lambda.pdf")
@@ -220,16 +208,12 @@ scores_lambda[ , {
       oma = c(5.1, 4.1, 3.1, 2.1))
   MAX = max(pei)
   for (ii in 1:nrow(dims)) {
-    x = dcast(.SD[dims[ii], on = c("delx", "dely")], 
+    x = dcast(.SD[dims_lambda[ii], on = c("delx", "dely")], 
               lambda ~ eta + lt, value.var = "pei")
     y = x[ , -1L]
-    matplot(x[[1L]], y, type = "l", lty = 1, col = cols[names(y)], lwd = 3,
+    matplot(x[[1L]], y, type = "l", lty = 1, lwd = 3,
             ylab = "", xlab = "", ylim = c(0, MAX), xaxt = "n", yaxt = "n")
     tile.axes(ii, 2L, 2L)
-    if (ii == 3L)
-      legend("topright", do.call(paste, c(tstrsplit(names(y), "_"),
-                                            list(sep = "/"))),
-             col = cols[names(y)], lwd = 3, title = "eta/lt", ncol = 2L)
     if (ii < 3L) mtext(paste0("E-W Cell Size: ", dims[ii, delx]), 
                              side = 3L, cex = .5)
           if (ii %% 2L == 0L) mtext(paste0("N-S Cell Size: ", dims[ii, dely]),
@@ -243,23 +227,22 @@ scores_lambda[ , {
   dev.off()
   }]
 
-timings[delx == 425 & dely == 600 & delta == 1 & l1 == 1e-5 & 
-          l2 == 1e-4 & k == 200 & t0 == 0 & p == .5
-        ][ , .(time = mean(time)), keyby = lambda
-           ][ , {
-             pdf("timings/vary_lambda.pdf")
-             plot(lambda, time, type = "l", col = "red", lwd = 3L,
-                  xlab = "lambda", ylab = "Total Time", ylim = c(0, MAXT),
-                  main = "Sensitivity of Run Time to Lambda")
-             dev.off()
-           }]
+pdf("timings/vary_lambda.pdf")
+boxplot(time ~ lambda, data = timings[delta == 1 & l1 == 1e-5 & l2 == 1e-4 & 
+                                   k == 200 & t0 == 0 & p == .5],
+        col = "red", xlab = "Lambda", ylab = "Total Time", 
+        main = "Sensitivity of Run Time to Lambda")
+dev.off()
+
 
 # P ####
 scores_p = 
-  scores[delta == 1 & k == 200 & l1 == 1e-5 & l2 == 1e-4 & lambda == .5 & 
-           t0 == 0][ , .(pei = max(pei), pai = max(pai)),
-                     by = .(delx, dely, eta, lt, p)]
-
+  scores[delx %in% c(250, 425) & dely %in% c(250, 425) & 
+           delta == 1 & l1 == 1e-5 & l2 == 1e-4 & lambda == .5 & 
+           t0 == 0 & k == 200][ , .(pei = max(pei), pai = max(pai)),
+                               by = .(delx, dely, eta, lt, p)]
+dims_p = scores_p[ , CJ(delx = delx, dely = dely, 
+                        unique = TRUE)][order(-dely, delx)]
 scores_p[ , {
   pdf("scores/vary_p.pdf")
   par(mfrow = c(uniqueN(delx), uniqueN(dely)),
@@ -267,16 +250,12 @@ scores_p[ , {
       oma = c(5.1, 4.1, 3.1, 2.1))
   MAX = max(pei)
   for (ii in 1:nrow(dims)) {
-    x = dcast(.SD[dims[ii], on = c("delx", "dely")], 
+    x = dcast(.SD[dims_p[ii], on = c("delx", "dely")], 
               p ~ eta + lt, value.var = "pei")
     y = x[ , -1L]
-    matplot(x[[1L]], y, type = "l", lty = 1, col = cols[names(y)], lwd = 3,
+    matplot(x[[1L]], y, type = "l", lty = 1, lwd = 3,
             ylab = "", xlab = "", ylim = c(0, MAX), xaxt = "n", yaxt = "n")
     tile.axes(ii, 2L, 2L)
-    if (ii == 3L)
-      legend("topright", do.call(paste, c(tstrsplit(names(y), "_"),
-                                            list(sep = "/"))),
-             col = cols[names(y)], lwd = 3, title = "eta/lt", ncol = 2L)
     if (ii < 3L) mtext(paste0("E-W Cell Size: ", dims[ii, delx]), 
                              side = 3L, cex = .5)
           if (ii %% 2L == 0L) mtext(paste0("N-S Cell Size: ", dims[ii, dely]),
@@ -290,16 +269,13 @@ scores_p[ , {
   dev.off()
   }]
 
-timings[delx == 250 & dely == 600 & delta == 1 & l1 == 1e-5 & 
-          l2 == 1e-4 & lambda == .5 & t0 == 0 & k == 200
-        ][ , .(time = mean(time)), keyby = p
-           ][ , {
-             pdf("timings/vary_p.pdf")
-             plot(p, time, type = "l", col = "red", lwd = 3L,
-                  xlab = "# Features", ylab = "Total Time",
-                  main = "Sensitivity of Run Time to p")
-             dev.off()
-           }]
+pdf("timings/vary_p.pdf")
+boxplot(time ~ p, data = timings[delta == 1 & l1 == 1e-5 & l2 == 1e-4 & 
+                                   lambda == .5 & t0 == 0 & k == 200 & p > .01],
+        col = "red", xlab = "p", ylab = "Total Time", 
+        main = "Sensitivity of Run Time to p")
+dev.off()
+
 
 # T0 ####
 scores_t0 = 
