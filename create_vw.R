dc52bd3cf40770210985717574858621bc98500f
ppau1 <pau.pereira.bcn@gmail.com>
2017-01-30 17:10:26 -0500

    kdes to vw/gen vars in csv files

--- create_vw.R
+++ create_vw.R
@@ -1,13 +1,14 @@
-<<<<<<< HEAD
 #!/usr/bin/env Rscript
 # Forecasting Crime in Portland
 # **     GPP Featurization      **
 # Michael Chirico, Seth Flaxman,
 # Charles Loeffler, Pau Pereira
 # cat("Setup...\t")
-# t0 = proc.time()["elapsed"]
+t0 = proc.time()["elapsed"]
 suppressMessages({
+  library(OGR)
   library(spatstat, quietly = TRUE)
+  library(splancs, quietly = TRUE)
   #data.table after spatstat to
   #  access data.table::shift more easily
   library(data.table, warn.conflicts = FALSE, quietly = TRUE)
@@ -18,22 +19,39 @@ set.seed(60251935)
 
 #inner parameters
 args = commandArgs(trailingOnly = TRUE)
-delx = as.integer(args[1L])
-dely = as.integer(args[2L])
-alpha = as.numeric(args[3L])
-eta = as.numeric(args[4L])
-lt = as.numeric(args[5L])
-features = as.integer(args[6L])
-l1 = as.numeric(args[7L])
-l2 = as.numeric(args[8L])
-lambda = as.numeric(args[9L])
-delta = as.numeric(args[10L])
-t0.vw = as.numeric(args[11L])
-pp = as.numeric(args[12L])
+# delx = as.integer(args[1L])
+# dely = as.integer(args[2L])
+# alpha = as.numeric(args[3L])
+# eta = as.numeric(args[4L])
+# lt = as.numeric(args[5L])
+# features = as.integer(args[6L])
+# l1 = as.numeric(args[7L])
+# l2 = as.numeric(args[8L])
+# lambda = as.numeric(args[9L])
+# delta = as.numeric(args[10L])
+# t0.vw = as.numeric(args[11L])
+# pp = as.numeric(args[12L])
+# 
+# #outer parameters
+# horizon = args[13L]
+# crime.type = args[14L]
+
+delx = 600
+dely = 600 
+alpha = 0
+eta = 1.5
+lt = 4
+features = 100
+l1 = 1e-5
+l2 = 1e-4
+lambda = .5
+delta = 1
+t0.vw = 0
+pp = .5
 
 #outer parameters
-horizon = args[13L]
-crime.type = args[14L]
+horizon = '2m'
+crime.type = 'all'
 
 #baselines for testing:
 # delx=dely=600;alpha=0;eta=3;lt=4
@@ -59,17 +77,16 @@ crime.file = switch(crime.type,
                     burglary = "crimes_bur.csv",
                     vehicle = "crimes_veh.csv")
 
+crime.shapefile = switch(crime.type,
+                    all = "crimes_all",
+                    street = "crimes_str",
+                    burglary = "crimes_bur",
+                    vehicle = "crimes_veh")
+
 crimes = fread(crime.file)
 crimes[ , occ_date := as.IDate(occ_date)]
-
 # trying to learn using only recent data for now
 crimes = crimes[occ_date >= '2015-09-01']
-=======
-library(data.table)
-library(spatstat)
-
-crimes = fread("crimes.csv")
->>>>>>> b8f95a315bd7efed3b06f0ddca66d5e14df0b0c7
 
 #record range here, so that
 #  we have the same range 
@@ -77,23 +94,66 @@ crimes = fread("crimes.csv")
 xrng = crimes[ , range(x_coordina)]
 yrng = crimes[ , range(y_coordina)]
 
-<<<<<<< HEAD
+# ============================================================================
+# GRID TOPOLOGY
+# Used to compute KDEs
+# Also create idx to reartange order of pixellate objects so they conform with
+# GT objects
+# ============================================================================
+# from pixel image create GridTopology
+pix <- crimes[occ_date <= end.date,
+              pixellate(
+                ppp(x=x_coordina, y=y_coordina, xrange=xrng, yrange=yrng, check=FALSE),
+                eps=c(x=delx, dely))]
+grdtop <- as(as.SpatialGridDataFrame.im(pix), "GridTopology")
+
+# create sp object of crimes
+prj = CRS("+init=epsg:2913")
+crimes.sp = with(crimes,
+                 SpatialPointsDataFrame(
+                    coords = cbind(x_coordina, y_coordina),
+                    data = crimes[, -c('x_coordina','y_coordina'), with=FALSE],
+                    proj4string = prj))
+
+# load  portland boundary
+# crimes.sp <- readOGR(dsn='data/combined', layer=crime.shapefile, verbose=FALSE)
+portland.bdy <- readOGR(dsn='data', layer='portland_boundary', verbose=FALSE)
+portland.bdy.coords <- portland.bdy.simp@polygons[[1L]]@Polygons[[1L]]@coords
+
+getGTindices <- function (gt) {
+  # Obtain indices to rearange data from image (eg. result frim pixellate)
+  # so that it conforms with data from GridTopology objects (eg. results
+  # from using spkernel2d).
+  # Input: gt is a grid topology.
+  # Returns an index.
+  dimx <- gt@cells.dim[1]
+  dimy <- gt@cells.dim[2]
+  idx <- 1:(dimx*dimy)
+  idx.mat <- matrix(idx, ncol = dimx)
+  idx.mat <- idx.mat[nrow(idx.mat):1,]
+  as.vector(t(idx.mat))
+}
+
+# index to rearange rows in pixellate objects
+idx.new <- getGTindices(grdtop)
+
+# ============================================================================
+# CREATE DATA TABLE OF CRIMES
+# aggregate at week-cell level
+# ============================================================================
+
 #**TO DO: Check this Issue to be sure 
 #  the cells are actually sorted by x,y
 #  https://github.com/spatstat/spatstat/issues/37
 incl_ids = 
-=======
-crimes_ever = 
->>>>>>> b8f95a315bd7efed3b06f0ddca66d5e14df0b0c7
   with(crimes, setDT(as.data.frame(pixellate(ppp(
     #pixellate counts dots over each cell,
     #  and appears to do so pretty quickly
     x = x_coordina, y = y_coordina,
-<<<<<<< HEAD
     xrange = xrng, yrange = yrng, check = FALSE),
     #this must be done within-loop
     #  since it depends on delx & dely
-    eps = c(delx, dely))))
+    eps = c(delx, dely)))[idx.new, ])
     #find cells that ever have a crime
   )[value > 0, which = TRUE]
 
@@ -106,13 +166,66 @@ crimes.grid.dt =
          as.data.table(pixellate(ppp(
            x = x_coordina, y = y_coordina,
            xrange = xrng, yrange = yrng, check = FALSE),
-           eps = c(x = delx, dely))),
+           eps = c(x = delx, dely)))[idx.new,],
          #subset to eliminate never-crime cells
          by = week_no][ , I := rowid(week_no)][I %in% incl_ids]
-rm(crimes, incl_ids)
 
 #can use this to split into train & test
 crimes.grid.dt[ , train := week_no > 0L]
+
+# ============================================================================
+# KDEs
+# ============================================================================
+
+compute.kde <- function (pts, grd=grdtop, h0 = 1000, poly=portland.bdy.coords) {
+  spkernel2d(pts=pts, poly=poly, h0=h0, grd=grd, kernel='quartic')
+}
+
+pts.selection <- function (pts, month, nb_days=7){
+  # pick random days from given month. Return sp object.
+  pts.month = pts[pts$month_no==1,]
+  days.month = sample(unique(pts.month$day_no), 7)
+  pts.month[pts.month$day_no %in% days.month, ]
+}
+
+compute.kde.list <- function (pts, months = 1:6) {
+     # compute kde for each month, on a random pick of days.
+     # return data.table, each col stores results for one month
+     sp.list = lapply(months, function(month) pts.selection(pts, month))
+     kdes = as.data.table(lapply(sp.list, compute.kde))
+     setnames(kdes, names(kdes), sapply(names(kdes), function (colname) gsub('V', 'kde', colname)))
+     kdes
+}
+
+kdes = compute.kde.list(crimes.sp)
+
+# sgdf <- SpatialGridDataFrame(grid = grdtop, kde)
+# as.data.table(sgdf)[crimes.grid.dt, on='I'] # sanity check
+
+# ============================================================================
+# SUBCATEGORIES - CALLGROUPS
+# Compute KDE for last mont for top three callgroups
+# ============================================================================
+
+# pick largest call groups
+callgroup.top = crimes[, .N, by=CALL_GROUP][order(-N)][1:3, CALL_GROUP]
+crimes.cgroup = lapply(callgroup.top, function (x) crimes.sp[crimes.sp$CALL_GROUP == x,])
+kdes.sub = lapply(crimes.cgroup, function (pts) compute.kde.list(pts, months=1))
+kdes.sub = as.data.table(kdes.sub)
+kdes.sub = setNames(kdes.sub, paste0('skde', 1:ncol(kdes.sub)))
+
+# combine normal kdes and sub-kdes
+kdes = cbind(kdes, kdes.sub)
+
+# add cell id
+kdes[, I := .I]
+
+# append kdes
+crimes.grid.dt <- kdes[crimes.grid.dt, on='I']
+
+# ============================================================================
+# PROJECTION
+# ============================================================================
 
 #project -- these are the omega * xs
 # t1 = proc.time()["elapsed"]
@@ -131,6 +244,13 @@ proj = crimes.grid.dt[ , cbind(x, y, week_no)] %*%
 phi.dt = with(crimes.grid.dt,
               data.table(v = value,
                          l = paste0(I, "_", week_no, "|")))
+
+# add KDEs to features matrix
+function (col) {
+  paste0
+}
+
+
 if (features > 500L) alloc.col(phi.dt, 3L*features)
 #create the features
 #  previously explored alternative:
@@ -257,49 +377,12 @@ params = paste(delx, dely, alpha, eta, lt, features, l1, l2,
 cat(params, "\n", sep = "", append = TRUE, file = ff)
 invisible(file.remove(cache, pred.vw))
 
-# t1 = proc.time()["elapsed"]
+t1 = proc.time()["elapsed"]
+ft = paste0("timings/", crime.type, "_", horizon, ".csv")
+if (!file.exists(ft)) 
+  cat("delx,dely,alpha,eta,lt,k,l1,l2,",
+      "lambda,delta,t0,p,time\n", sep = "", file = ft)
+params = paste(delx, dely, alpha, eta, lt, features, l1, l2,
+               lambda, delta, t0.vw, pp, t1 - t0, sep = ",")
+cat(params, "\n", sep = "", append = TRUE, file = ft)
 # cat(sprintf("%3.0fs", t1 - t0), "\n")
-=======
-    xrange =  xrng, yrange = yrng),
-    #we can easily make these command line arguments
-    dimyx = c(y = 600, x = 600)))))
-#record order for more reliable merging below
-crimes_ever[order(x, y), I := .I]
-#eliminate no-crime cells
-crimes_ever = crimes_ever[value > 0]
-
-#now subset to desired date range
-crimes = crimes[as.IDate(occ_date) %between%
-                  c("2016-02-22", "2016-02-28")]
-
-#repeat over-cell counting
-crimes.grid.dt = 
-  with(crimes, setDT(as.data.frame(pixellate(ppp(
-    x = x_coordina, y = y_coordina,
-    xrange = xrng, yrange = yrng),
-    dimyx = c(y = 600, x = 600)))))
-crimes.grid.dt[order(x, y), I := .I]
-
-#subset to eliminate never-crime cells
-crimes.grid.dt = 
-  crimes.grid.dt[crimes_ever, .(x, y, value), on = "I"]
-
-#lengthscale and feature count
-l = 1800; k = 200
-
-#project -- these are the omega * xs
-proj = crimes.grid.dt[ , cbind(x, y)] %*% 
-  matrix(rnorm(2*k), nrow = 2L) / l
-
-#create the features
-phi = cbind(cos(proj), sin(proj))/sqrt(k)
-
-#convert to data.table for ease
-phi.dt = as.data.table(phi)
-
-fwrite(phi.dt[ , c(list(paste0(
-  crimes.grid.dt$value, " |")), lapply(
-    names(.SD), function(jj)
-      paste0(jj, ":", get(jj))))], 
-  "test.vw", sep = " ", col.names = FALSE, quote = FALSE)
->>>>>>> b8f95a315bd7efed3b06f0ddca66d5e14df0b0c7
