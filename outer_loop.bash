0705b802cc5072c6b29307ebaf5726966ce83db3
Michael Chirico <MichaelChirico4@gmail.com>
2017-01-31 19:25:33 -0500

    outer loop for best-of runs

--- outer_loop.bash
+++ outer_loop.bash
@@ -1,7 +1,22 @@
 #!/bin/sh
 
-qsub vary_features.sh
-qsub vary_l1.sh
-qsub vary_l2.sh
-qsub vary_lambda.sh
-qsub vary_p.sh
+qsub best_of_1m_all.sh
+qsub best_of_1m_burglary.sh
+qsub best_of_1m_street.sh
+qsub best_of_1m_vehicle.sh
+qsub best_of_1w_all.sh
+qsub best_of_1w_burglary.sh
+qsub best_of_1w_street.sh
+qsub best_of_1w_vehicle.sh
+qsub best_of_2m_all.sh
+qsub best_of_2m_burglary.sh
+qsub best_of_2m_street.sh
+qsub best_of_2m_vehicle.sh
+qsub best_of_2w_all.sh
+qsub best_of_2w_burglary.sh
+qsub best_of_2w_street.sh
+qsub best_of_2w_vehicle.sh
+qsub best_of_3m_all.sh
+qsub best_of_3m_burglary.sh
+qsub best_of_3m_street.sh
+qsub best_of_3m_vehicle.sh
