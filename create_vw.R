289da359e95fbc7afe05229f012bc16137d743a2
Michael Chirico <MichaelChirico4@gmail.com>
2017-02-11 17:51:47 -0500

    turn off test params

--- create_vw.R
+++ create_vw.R
@@ -34,12 +34,12 @@ crime.type = args[9L]
 horizon = args[10L]
 
 # baselines for testing:
-delx=dely=250;alpha=0;eta=1.5;lt=4
-features=10;kde.bw=250;kde.lags=6
-horizon='1w';crime.type='all'
-cat("**********************\n",
-    "* TEST PARAMETERS ON *\n",
-    "**********************\n")
+# delx=dely=250;alpha=0;eta=1.5;lt=4
+# features=10;kde.bw=250;kde.lags=6
+# horizon='1w';crime.type='all'
+# cat("**********************\n",
+#     "* TEST PARAMETERS ON *\n",
+#     "**********************\n")
 
 aa = delx*dely #forecasted area
 lx = eta*delx
