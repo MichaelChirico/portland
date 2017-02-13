4ee4a522752dbdbb4ff51e949fa670125ffab6af
Michael Chirico <MichaelChirico4@gmail.com>
2017-02-12 19:44:52 -0500

    expanding local_setup details

--- local_setup.R
+++ local_setup.R
@@ -1,13 +1,23 @@
 hostname = Sys.info()["nodename"]
 cat("running on ", hostname, "\n")
 
-if(grep("ziz",hostname)) { # Seth's setup
+if(grepl("ziz",hostname)) { # Seth's setup
     source("cleanup.R")
     tdir = "/data/localhost/not-backed-up/flaxman"
     job_id = paste0("_",Sys.getenv("SLURM_JOB_ID"))
     path_to_vw = "./vw"
-} else  {
-    tdir = "delete_me"
+} else if (grepl('michael', hostname)) {
+    tdir = "/tmp"
     job_id = ""
     path_to_vw = "vw"
+} else if (grepl('gpc', hostname)) {
+  #should start pushing to loefllerlab which
+  #  apparently has much higher hard disk cap
+  tdir = "delete_me"
+  job_id = ""
+  path_to_vw = "vw"
+} else {
+  tdir = "delete_me"
+  job_id = ""
+  path_to_vw = "vw" 
 }
