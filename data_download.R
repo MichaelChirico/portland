e773787965620a8793f98869b3d5dcfe972ab86f
Michael Chirico <MichaelChirico4@gmail.com>
2017-02-13 18:28:51 -0500

    fixed data downloader

--- data_download.R
+++ data_download.R
@@ -9,14 +9,14 @@ gc()
 library(rvest) 
 
 # Official Contest Data
-URL = "http://www.nij.gov/funding/Pages/" %+% 
-  "fy16-crime-forecasting-challenge.aspx"
+URL = paste0("http://www.nij.gov/funding/Pages/",
+             "fy16-crime-forecasting-challenge.aspx")
 
 data_links = read_html(URL) %>% 
   #Get all <a href=...> tags, convert to text
   html_nodes(xpath = "//a/@href") %>% html_text() %>%
   #All data files end in .zip
-  grep("\\.zip$", ., value = TRUE) %>% 
+  grep("Data.zip", ., fixed = TRUE, value = TRUE) %>% 
   #URLS are internal to the nij.gov site, so prepend
   paste0("www.nij.gov", .)
 
@@ -25,7 +25,7 @@ for (uu in data_links) {
   tmp = tempfile()
   # method = "auto" -> method = "internal" was
   #  returning an error (?)
-  download.file(uu, tmp, method = "curl")
-  unzip(tmp, exdir = wds["data"])
+  download.file(uu, tmp, method = 'wget')
+  unzip(tmp, exdir = "data")
   unlink(tmp)
 }
