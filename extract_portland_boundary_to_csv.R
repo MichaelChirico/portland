24cabc104523a1680c781f262a173052f349eef3
Michael Chirico <MichaelChirico4@gmail.com>
2017-02-14 19:01:47 -0500

    no longer use questionable-origin portland boundary file; go directly from competition-provided shapefiles

--- extract_portland_boundary_to_csv.R
+++ extract_portland_boundary_to_csv.R
@@ -1,11 +1,37 @@
-library(maptools)
 library(rgeos)
 library(data.table)
+library(rgdal)
 
-portland.bdy <- gBuffer(
-  readShapePoly("data/portland_boundary", 
-                proj4string = CRS("+init=epsg:2913")), 
-  width = 500
+police_districts = 
+  readOGR('data', 'Portland_Police_Districts', 
+          p4s = "+init=epsg:2913")
+
+portland = gUnaryUnion(gBuffer(
+  #buffer to eliminate sand grain holes
+  #  (1e6 by trial and elimination)
+  police_districts, width = 1e6*.Machine$double.eps
+))
+
+#number of polygon constituents
+#  (including holes, islands)
+npoly = length(portland@polygons[[1L]]@Polygons)
+out = which.max(sapply(portland@polygons[[1L]]@Polygons,
+                       #ringDir = 1: outer boundary;
+                       #ringDir = 2: inner boundary (hole)
+                       function(p) if (p@ringDir > 0) nrow(p@coords) else 0))
+#extract the actual boundary polygon to a
+#  new object (there's gotta be a better way??)
+portland.boundary = 
+  SpatialPolygons(list(Polygons(list(
+    portland@polygons[[1L]]@Polygons[[out]]
+  ), ID = 'boundary')), proj4string = CRS(proj4string(portland)))
+
+#buffer (unbuffered was causing numerical issues)
+portland.boundary.buffer = gBuffer(
+  portland.boundary, width = 500
 )
-fwrite(as.data.table(portland.bdy@polygons[[1L]]@Polygons[[1L]]@coords),
-       'data/portland_coords.csv')
+
+#only really need the coordinates
+fwrite(as.data.table(
+  portland.boundary.buffer@polygons[[1L]]@Polygons[[1L]]@coords),
+  'data/portland_coords.csv')
