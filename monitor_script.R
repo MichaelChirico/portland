2137eb9d30b57224e3bfdab8bdf3511bc0137121
Michael Chirico <MichaelChirico4@gmail.com>
2017-02-27 11:25:24 -0500

    adjust monitor script

--- monitor_script.R
+++ monitor_script.R
@@ -5,9 +5,16 @@ names(fn) = gsub('.*ar_ws_my_(.*)arws.*', '\\1', fn)
 
 x = rbindlist(lapply(fn, fread), idcol = 'hc')
 
-cat('**OPTIMA FROM', 
-    uniqueN(x, by = c('delx', 'dely', 'eta', 'lt', 'theta', 
-                      'k', 'kde.bw', 'kde.lags', 'kde.win')),
-    'RUNS**\n\n')
+x[ , run_no := .GRP, 
+   by =  c('delx', 'dely', 'eta', 'lt', 'theta', 
+           'k', 'kde.bw', 'kde.lags', 'kde.win')]
 
-x[ , .SD[unique(c(which.max(pei), which.max(pai)))], by = hc]
+x[ , param_no := .GRP, 
+   by =  setdiff(names(x), c('train_set', 'pei', 'pai'))]
+
+cat('**OPTIMA FROM', max(x$run_no), 'RUNS**\n\n')
+
+idx = x[ , .(mean(pei), mean(pai)), by = param_no
+         ][unique(c(which.max(V1), which.max(V2))), param_no]
+
+x[param_no == idx]
