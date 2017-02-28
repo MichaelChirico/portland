bd50d8b35e28669a525a4d4a68c0983c28269d57
Michael Chirico <MichaelChirico4@gmail.com>
2017-02-27 19:39:47 -0500

    re-jig competition pipeline to work with AR winter-spring model

--- competition_output.R
+++ competition_output.R
@@ -15,46 +15,53 @@
 library(spatstat)
 library(splancs)
 library(rgeos)
-library(rgdal)
 library(data.table)
-library(foreach)
 library(maptools)
-library(zoo)
 
 #make sure we use the same seed
 #  as evaluation_params.R
 set.seed(60251935)
 
-args = 
-  read.table(text = paste(commandArgs(trailingOnly = TRUE), collapse = '\t'),
-             sep = '\t', stringsAsFactors = FALSE)
-names(args) = c('delx', 'dely', 'alpha', 'eta', 'lt', 'theta',
-                'features', 'kde.bw', 'kde.lags', 'l1', 'l2',
-                'lambda', 'delta', 'T0', 'pp',
-                'crime.type', 'horizon')
-attach(args)
+#add/remove ! below to turn testing on/off
+..testing = 
+  !FALSE
 
-# baselines for testing:
-# delx=683;dely=487;alpha=0.616;eta=.948;lt=5.12;theta=pi/4
-# features=25;kde.bw=313;kde.lags=2;
-# l1=1e-5;l2=1e-4;lambda=.5;delta=1;T0=0;pp=.5
-# crime.type='all';horizon='1m'
-# cat("**********************\n",
-#     "* TEST PARAMETERS ON *\n",
-#     "**********************\n")
+if (..testing) {
+  # delx=600;dely=600;alpha=0;eta=1;lt=14;theta=0
+  # features=250;l1=1e-5;l2=1e-4;
+  # kde.bw=500;kde.lags=6;kde.win = 3
+  # crime.type='all';horizon='2m'
+  args = read.table(text = 'all	3m	531	605	0	3.187105	5.709805	0.508646	225	0	0	443.786032	11	86.66031',
+                    sep = '\t', col.names = c('crime.type', 'horizon', 'delx', 'dely',
+                                              'alpha', 'eta', 'lt',
+                                              'theta', 'features', 'l1', 'l2', 
+                                              'kde.bw', 'kde.lags', 'kde.win'),
+                    stringsAsFactors = FALSE)
+  attach(args)
+
+  cat("**********************\n",
+      "* TEST PARAMETERS ON *\n",
+      "**********************\n")
+} else {
+  # each argument read in as a string in a character vector;
+  # would rather have them as a list. basically do
+  # that by converting them to a form read.table
+  # understands and then attaching from a data.frame
+  args = read.table(text = paste(commandArgs(trailingOnly = TRUE),
+                                 collapse = '\t'),
+                    stringsAsFactors = FALSE,
+                    col.names = c('delx', 'dely', 'alpha', 'eta', 'lt',
+                                  'theta', 'features', 'l1', 'l2', 
+                                  'kde.bw', 'kde.lags', 'kde.win', 
+                                  'crime.type', 'horizon'))
+  attach(args)
+}
+
+incl_mos = c(10L, 11L, 12L, 1L, 2L, 3L)
 
 aa = delx*dely
-lx = eta*delx
-ly = eta*dely
-
-week_0 = 0L
-lag.range = week_0 + 
-  c(switch(horizon, '1w' = 54L, '2w' = 53L,
-           '1m' = 50L, '2m' = 46L, '3m' = 42L), 80L)
-
-recent = week_0 + 
-  c(switch(horizon, '1w' = 0, '2w' = -1L,
-           '1m' = -4L, '2m' = -8L, '3m' = -12L), 26L)
+lx = eta*250
+ly = eta*250
 
 crime.file = switch(crime.type,
                     all = "crimes_all.csv",
@@ -109,113 +116,98 @@ incl_ids =
     eps = c(delx, dely)))[idx.new, ]
   )[value > 0, which = TRUE]
 
-#join some missing crimes from unseen dates
-seq_rng = function(rng) seq.int(rng[1L], rng[2L])
-fake_crimes = 
-  data.table(week_no = c(seq_rng(lag.range),
-                         seq_rng(recent)))
-crimes = crimes[fake_crimes, on = 'week_no']
-#crimes.sp throws a hissy fit if fed NA coordinates
-real_crimes = crimes[!is.na(x_coordina), which = TRUE]
+pd_length = switch(horizon, 
+                   '1w' = 7L, '2w' = 14L, '1m' = 31,
+                   '2m' = 61L, '3m' = 92L) 
+one_year = switch(horizon, 
+                  '1w' = 52L, '2w' = 26L, '1m' = 12L,
+                  '2m' = 6L, '3m' = 4L)
 
-#while we don't have the full data set, we're missing
-#  data from the end of February -- to avoid this
-#  mucking up the test process, we'll pad the
-#  current data with each cell's ancestor
-pad = crimes[is.na(x_coordina) & week_no > 0, 
-             week_no + 52L]
+n_pds = 5L*one_year
+
+crimes[ , occ_date_int := unclass(occ_date)]
+unq_crimes = crimes[ , unique(occ_date_int)]
+
+march117 = unclass(as.IDate('2017-03-01'))
+start = march117 - (seq_len(n_pds) - 1L) * pd_length
+start = start[month(as.IDate(start, origin = '1970-01-01')) %in% incl_mos]
+end = start + pd_length - 1L
+windows = data.table(start, end, key = 'start,end')
+
+crime_start_map = data.table(occ_date_int = unq_crimes)
+crime_start_map[ , start_date := 
+                   foverlaps(data.table(start = occ_date_int, 
+                                        end = occ_date_int),
+                             windows)$start]
+
+crimes[crime_start_map, start_date := i.start_date,
+       on = 'occ_date_int']
+
+X = crimes[!is.na(start_date), as.data.table(pixellate(ppp(
+  x = x_coordina, y = y_coordina,
+  xrange = xrng, yrange = yrng, check = FALSE),
+  #reorder using GridTopology - im mapping
+  eps = c(x = delx, dely)))[idx.new],
+  #subset to eliminate never-crime cells
+  keyby = start_date][ , I := rowid(start_date)][I %in% incl_ids]
 
 crimes.sp =
   SpatialPointsDataFrame(
-    coords = crimes[(real_crimes), cbind(x_coordina, y_coordina)],
-    data = crimes[(real_crimes), -c('x_coordina', 'y_coordina')],
+    coords = crimes[ , cbind(x_coordina, y_coordina)],
+    data = crimes[ , -c('x_coordina', 'y_coordina')],
     proj4string = prj
   )
 
-crimes.grid.dt =
-  crimes[week_no %between% recent, 
-         as.data.table(pixellate(ppp(
-           x = x_coordina, y = y_coordina,
-           xrange = xrng, yrange = yrng, check = FALSE),
-           eps = c(delx, dely)))[idx.new],
-         by = week_no][ , I := rowid(week_no)][I %in% incl_ids]
+crimes.sp@data = setDT(crimes.sp@data)
+#for faster indexing
+setkey(crimes.sp@data, occ_date_int)
 
-#if we have full data, this step will do nothing
-crimes.pad = 
-  crimes[week_no %in% pad,
-         as.data.table(pixellate(ppp(
-           x = x_coordina, y = y_coordina,
-           xrange = xrng, yrange = yrng, check = FALSE),
-           eps = c(delx, dely)))[idx.new],
-         by = .(week_no = week_no - 52L)
-         ][ , I := rowid(week_no)][I %in% incl_ids]
-crimes.grid.dt[crimes.pad, value := i.value, on = c('week_no', 'I')]
+future = 
+  #Add one missing row for each cell corresponding to start date March 1, 2017
+  unique(X, by = 'I')[ , c('start_date', 'value') := .(march117, NA_integer_)][]
+X = rbind(X, future)
 
-compute.kde <- function(pts, month)
-  spkernel2d(pts = pts[pts$month_no == month, ],
-             poly = portland_r, h0 = kde.bw, grd = grdtop)
-
-compute.lag = function(pts, week_no)
-  spkernel2d(pts = pts[pts$week_no %between%
-                         (week_no + c(50L, 54L)), ],
-             poly = portland_r, h0 = kde.bw, grd = grdtop)
-
-compute.kde.list <- function (pts, months = seq_len(kde.lags) + 12L)
-  lapply(setNames(months, paste0('kde', months)),
-         function(month) compute.kde(pts, month))
-
-kdes = setDT(compute.kde.list(crimes.sp))
-
-## **TO DO: deal with this properly
-callgroup.top = 
-  fread('top_callgroups_by_crime.csv')[crime == crime.type, cg]
-
-if (length(callgroup.top)) {
-  crimes.cgroup = lapply(callgroup.top, function(cg) 
-    crimes.sp[crimes.sp$call_group_type == cg, ])
-  
-  kdes.sub = setDT(sapply(crimes.cgroup, function(pts) 
-    compute.kde.list(pts, months = 13L)))
-  
-  setnames(kdes.sub, paste0('cg.kde', seq_len(ncol(kdes.sub))))
-  
-  # combine normal kdes and sub-kdes
-  kdes = cbind(kdes, kdes.sub)
+compute.kde <- function(pts, start, lag.no) {
+  idx = pts@data[occ_date_int %between% 
+                   (start - kde.win*lag.no + c(0, kde.win - 1L)), which = TRUE]
+  if (!length(idx)) return(rep(0, length(incl_ids)))
+  kde = spkernel2d(pts = pts[idx, ],
+                   poly = portland_r, h0 = kde.bw, grd = grdtop)[incl_ids]
 }
 
-kdes[ , I := .I]
+start_lag = CJ(start = start, lag = seq_len(kde.lags))
 
-crimes.grid.dt = kdes[crimes.grid.dt, on = 'I']
+RHS = start_lag[, c(I = list(incl_ids),
+                    lapply(setNames(lag, paste0('lag', lag)), compute.kde, 
+                           pts = crimes.sp, start = .BY$start)),
+                by = start]
 
-crimes.grid.dt[ , lg.kde := {
-  kde = compute.lag(crimes.sp, .BY$week_no)
-  idx = data.table(kde, I = seq_len(length(kde)))[.SD, on = 'I', which = TRUE]
-  kde[idx]
-}, by = week_no]
+X = X[RHS, on = c(start_date = 'start', 'I')]
 
-crimes.grid.dt[ , train := week_no > week_0]
+X[ , train := start_date != march117]
 
-proj = crimes.grid.dt[ , cbind(x, y, week_no)] %*%
+proj = X[ , cbind(x, y, start_date)] %*% 
   (matrix(rt(3L*features, df = 2.5), nrow = 3L)/c(lx, ly, lt))
 
-nms = setNames(nm = names(crimes.grid.dt))
-incl.kde = grep("^kde", nms, value = TRUE)
-incl.cg = grep("^cg.", nms, value = TRUE)
+incl = setNames(nm = names(X))
+incl.kde = grep("^lag", incl, value = TRUE)
 
 phi.dt =
-  crimes.grid.dt[ , {
-    coln_to_vw = function(vn) {
+  X[ , {
+    coln_to_vw = function(vn) { 
       V = get(vn)
-      val = V * 10^(abs(round(mean(log10(V[V>0])))))
+      #to assure maximum comparability to the data model used
+      #  in training, be sure to multiply by the same factor
+      trainV = V[start_date <= march117 - one_year*pd_length & V > 0]
+      val = V * 10^(abs(round(mean(log10(trainV)))))
+      if (any(is.nan(val)))
+        stop('NaNs detected! Current parameters:',
+             paste(args, collapse = '/'))
       sprintf("%s:%.5f", vn, val)
     }
-    c(list(v = value,
-           l = paste0(I, "_", week_no, "|kdes")),
+    c(list(v = value, 
+           l = paste0(I, "_", start_date, "|kdes")), 
       lapply(incl.kde, coln_to_vw),
-      list(cg_namespace = if (length(incl.cg)) '|cgkde'),
-      lapply(incl.cg, coln_to_vw),
-      list(lag_namespace = '|lgkde',
-           kdel = coln_to_vw('lg.kde')),
       list(rff_namespace = '|rff'))
   }]
 
@@ -223,60 +215,59 @@ if (features > 500L) invisible(alloc.col(phi.dt, 3L*features))
 fkt = 1/sqrt(features)
 for (jj in 1L:features) {
   pj = proj[ , jj]
-  set(phi.dt, j = paste0(c("cos", "sin"), jj),
+  set(phi.dt, j = paste0(c("cos", "sin"), jj), 
       value = list(sprintf("cos%i:%.5f", jj, fkt*cos(pj)),
                    sprintf("sin%i:%.5f", jj, fkt*sin(pj))))
 }
 rm(proj)
 
 source("local_setup.R")
-train.vw = tempfile(tmpdir = tdir, pattern = "train")
-test.vw = tempfile(tmpdir = tdir, pattern = "test")
-cache = paste0(train.vw, '.cache')
-pred.vw = tempfile(tmpdir = tdir, pattern = "predict")
-fwrite(phi.dt[crimes.grid.dt$train], train.vw,
-       sep = " ", quote = FALSE, col.names = FALSE,
-       showProgress = FALSE)
-fwrite(phi.dt[!crimes.grid.dt$train], test.vw,
-       sep = " ", quote = FALSE, col.names = FALSE,
-       showProgress = FALSE)
-
-crimes.grid.dt = crimes.grid.dt[(!train)]
-rm(phi.dt)
-
 which.round = function(x)
   if (x > 0) {if (x < 1) round else floor} else ceiling
-
 n.cells = as.integer(which.round(alpha)(6969600*(1+2*alpha)/aa))
 
+filename = paste('output',crime.type,horizon,sep = '_')
+train.vw = paste(paste0(tdir,'/train'), filename, sep='_')
+test.vw = paste(paste0(tdir,'/test'), filename, sep='_')
+cache = paste0(train.vw, '.cache')
+pred.vw = paste(paste0(tdir,'/pred'), filename, sep='_')
+fwrite(phi.dt[X$train], train.vw,
+       sep = " ", quote = FALSE, col.names = FALSE,
+       showProgress = FALSE)
+fwrite(phi.dt[!X$train], test.vw,
+       sep = " ", quote = FALSE, col.names = FALSE,
+       showProgress = FALSE)
+
 model = tempfile(tmpdir = tdir, pattern = "model")
-system(paste(path_to_vw, '--loss_function poisson --l1', l1, '--l2', l2,
-             '--learning_rate', lambda,
-             '--decay_learning_rate', delta,
-             '--initial_t', T0, '--power_t', pp, train.vw,
-             '--cache_file', cache, '--passes 200 -f', model))
+
+call.vw = paste(path_to_vw, '--loss_function poisson --l1', l1, 
+                '--l2', l2, train.vw, '--cache_file', cache, 
+                '--passes 200 -f', model)
+system(call.vw)
 invisible(file.remove(train.vw))
+
 system(paste(path_to_vw, '-t -i', model, '-p', pred.vw,
              test.vw, '--loss_function poisson'))
-invisible(file.remove(model, test.vw))
+invisible(file.remove(model))
 
 preds =
-  fread(pred.vw, sep = " ", header = FALSE, col.names = c("pred", "I_wk"))
+  fread(pred.vw, sep = " ", header = FALSE, col.names = c("pred", "I_start"))
 invisible(file.remove(pred.vw))
-preds[ , c("I", "week_no", "I_wk") :=
-         c(lapply(tstrsplit(I_wk, split = "_"), as.integer),
+
+preds[ , c("I", "start_date", "I_start") :=
+         c(lapply(tstrsplit(I_start, split = "_"), as.integer),
            list(NULL))]
 
-crimes.grid.dt[preds, pred.count := exp(i.pred), on = c("I", "week_no")]
+X[preds, pred.count := exp(i.pred), on = c("I", "start_date")]
 rm(preds)
 
-hotspot.ids =
-  crimes.grid.dt[ , .(tot.pred = sum(pred.count)), by = I
-                  ][order(-tot.pred)[1L:n.cells], I]
+ranks = 
+  X[(!train), .(tot.pred = sum(pred.count)), by = I
+    ][order(-tot.pred), .(I, rank = .I)]
 
 #define hotspots on grid's SPDF
 #  +() to force integer per guidelines
-grdSPDF$hotspot = +(grdSPDF$I %in% hotspot.ids)
+grdSPDF$hotspot = +(grdSPDF$I %in% ranks[rank <= n.cells, I])
 
 #reverse rotation -- rotated points to
 #  fit grid, now rotate grid to fit
@@ -306,6 +297,12 @@ proj4string(grdSPDF) = prj
 
 #add area per contest guidelines
 grdSPDF$area = gArea(grdSPDF, byid = TRUE)
+
+#be sure shapefiles fit contest guidelines
+stopifnot(gArea(grdSPDF[grdSPDF$hotspot == 1, ]) %between%
+            c(5280^2/4, 3*5280^2/4))
+stopifnot(gArea(grdSPDF) %between%
+            c(147.69*5280^2, 147.73*5280^2))
 
 out.horizon = switch(horizon, '1w' = '1WK', '2w' = '2WK',
                      '1m' = '1MO', '2m' = '2MO', '3m' = '3MO')
