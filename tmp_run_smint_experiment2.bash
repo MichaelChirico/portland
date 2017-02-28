545b8c66180ab444f18194a3149f2da2eff5f207
Tony Vo <4.tonyvo@gmail.com>
2017-02-28 00:38:00 -0500

    sdded pearmint constraints/running all experiments in aws

--- tmp_run_smint_experiment2.bash
+++ tmp_run_smint_experiment2.bash
@@ -9,21 +9,25 @@ smintdir=/opt/pkgs/spearmint/Spearmint/spearmint
 crime=$1
 horizon=$2
 score=$3
+constr=$4
+
 
 # 1. create an experiment directory
 echo '1. create an experiment directory'
 # --------------------------------------------------------
-newdir=$experdir/smint-$crime-$horizon-$score
-if [ -d "$newdir" ]; then
-	echo 'Removing exising experiment directory...Hope nothing important was there!'
-	echo 'Bye bye to' "$newdir"
-	if [ -d "$newdir"/output ]; then
-		echo 'CLEANING UP OUTPUT FILE!'
-		$smintdir/cleanup.sh "$newdir"
-	fi
-	rm -rf "$newdir"  #<--- not working (?!)
-	# mv $newdir $experdir/delete_me/smint-$crime-$horizon-$score
+if [ -n $constr ]; then # constrained
+	echo 'Running CONSTRAINED experiment. HERE WE GO!'
+ 	newdir=$experdir/smint-$crime-$horizon-$score-constr	
+else
+	newdir=$experdir/smint-$crime-$horizon-$score
 fi
+
+if [ -d $newdir ]; then
+	echo 'Removing exising experiment directory...Hope nothing important was there!'
+	# sudo rm -rf $newdir  <--- not working (?!)
+	mv $newdir $experdir/delete_me/smint-$crime-$horizon-$score
+fi
+
 mkdir $newdir
 
 # 2. copy template in experiment dir
@@ -38,20 +42,31 @@ echo '3. make experiment file'
 # the model. It tells it crime, horizon and score.
 # --------------------------------------------------------
 cd $newdir
-touch  $crime-$horizon-$score.exper
-# python make_experiment_file.py $crime $horizon $score
+python make_experiment_file.py $crime $horizon $score
+
+# poor way of changing config file to constrained one
+# should write python script to edit config file and add constraint code...
+if [ -n $constr ]; then # true if string constr is nonzero
+    echo "Changing config to constrained..."
+    mv config.json config.json.dontuse
+	mv config.json.constr config.json
+fi
 
 # 4. change experiment name in config.json
 echo '4. change experiment name in config.json'
 # --------------------------------------------------------
-python change_exper_name.py $crime-$horizon-$score
+if [ -n $constr ]; then # true if string constr is nonzero
+	python change_exper_name.py $crime-$horizon-$score
+else
+  	python change_exper_name.py $crime-$horizon-$score
+fi
 
 # 5. run experiment
 echo '5. run experiment'
 # --------------------------------------------------------
-if [ -d "$newdir/output" ]
+if [ -d $newdir/output ]
 	then
 		echo 'CLEANING UP OUTPUT FILE!'
-		$smintdir/cleanup.sh "$newdir"
+		$smintdir/cleanup.sh $newdir
 fi	
 python $smintdir/main.py $newdir
